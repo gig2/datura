6b56aee059cd93680acce5b7b1e79a2f661d9b86
stepharc <stephane.arcellier@hotmail.fr>
2018-12-16 09:57:05 +0100

    perspective_and_lookAt

--- src/main.cpp
+++ src/main.cpp
@@ -11,9 +11,11 @@ int main( int argc, char *argv[] )
     MainWindow w;
     w.show();
 
-    /*Manualtrans mt = Manualtrans();
+    Manualtrans mt = Manualtrans();
     //glm::mat4 mtest = mt.translate(glm::mat4(), glm::vec3(10,0,0));
-    glm::mat4 mtest = mt.scale(glm::vec3(2.0f, 2.0f, 2.0f));
+    //glm::mat4 mtest = mt.scale(glm::vec3(2.0f, 2.0f, 2.0f));
+    //glm::mat4 mtest = mt.lookAt(glm::vec3(0,0,-15), glm::vec3(0,0,0), glm::vec3(0,1,0));
+    glm::mat4 mtest = mt.perspective(35.f, 1.f, 0.1f, 100.f);
     glm::vec4 vtest = mt.transformVector(mtest, glm::vec4(2.1f, 1.5f, 0.7f, 0));
 
     for(int i = 0 ; i < 4; i++){
@@ -26,7 +28,7 @@ int main( int argc, char *argv[] )
 
     for(int i = 0; i < 4; i++){
         std::cout << vtest[i] << std::endl;
-    }*/
+    }
 
     return a.exec();
 }
