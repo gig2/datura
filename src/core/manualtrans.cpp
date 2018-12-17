909a27e826e351baba8dd8c1dc0828a359d624a8
Thibault Payet <monwarez@gmail.com>
2018-12-17 11:03:14 +0100

    fix perspective

--- src/core/manualtrans.cpp
+++ src/core/manualtrans.cpp
@@ -36,44 +36,44 @@ glm::mat4 Manualtrans::translate( glm::vec3 const& direction ) const
     return matrix_to_return;
 }
 
-//Defines a view matrix, transform from world space to position space.
-//Source : https://stackoverflow.com/questions/21830340/understanding-glmlookat
+// Defines a view matrix, transform from world space to position space.
+// Source : https://stackoverflow.com/questions/21830340/understanding-glmlookat
 glm::mat4 Manualtrans::lookAt( glm::vec3 const& position, glm::vec3 const& target,
                                glm::vec3 const& up ) const
 {
-    glm::mat4 matrix_to_return;
+    glm::mat4 matrix_to_return{0.f};
     glm::vec3 X, Y, Z;
 
-    //New coordinate system, X points to the left, Y up and Z out of the view.
-    Z = position - target; //line of sight
-    //Get a vector with the same direction as Z but with norm of 1.
-    glm::normalize(Z);
+    // New coordinate system, X points to the left, Y up and Z out of the view.
+    Z = position - target; // line of sight
+    // Get a vector with the same direction as Z but with norm of 1.
+    glm::normalize( Z );
     Y = up;
-    X = glm::cross(Y, Z);
+    X = glm::cross( Y, Z );
 
     // Cross product of Z and X, that is to say get the vector which
     // is perdendicular to Z and X.
-    Y = glm::cross(Z, X);
-    glm::normalize(X);
-    glm::normalize(Y);
+    Y = glm::cross( Z, X );
+    glm::normalize( X );
+    glm::normalize( Y );
 
-    matrix_to_return[0][0] = X.x;
-    matrix_to_return[1][0] = X.y;
-    matrix_to_return[2][0] = X.z;
+    matrix_to_return[ 0 ][ 0 ] = X.x;
+    matrix_to_return[ 1 ][ 0 ] = X.y;
+    matrix_to_return[ 2 ][ 0 ] = X.z;
     // Dot product of X and position, that is to say X * position.
-    matrix_to_return[3][0] = -glm::dot(X, position);
-    matrix_to_return[0][1] = Y.x;
-    matrix_to_return[1][1] = Y.y;
-    matrix_to_return[2][1] = Y.z;
-    matrix_to_return[3][1] = -glm::dot(Y, position);
-    matrix_to_return[0][2] = Z.x;
-    matrix_to_return[1][2] = Z.y;
-    matrix_to_return[2][2] = Z.z;
-    matrix_to_return[3][2] = -glm::dot(Z, position);
-    matrix_to_return[0][3] = 0;
-    matrix_to_return[1][3] = 0;
-    matrix_to_return[2][3] = 0;
-    matrix_to_return[3][3] = 1.0f;
+    matrix_to_return[ 3 ][ 0 ] = -glm::dot( X, position );
+    matrix_to_return[ 0 ][ 1 ] = Y.x;
+    matrix_to_return[ 1 ][ 1 ] = Y.y;
+    matrix_to_return[ 2 ][ 1 ] = Y.z;
+    matrix_to_return[ 3 ][ 1 ] = -glm::dot( Y, position );
+    matrix_to_return[ 0 ][ 2 ] = Z.x;
+    matrix_to_return[ 1 ][ 2 ] = Z.y;
+    matrix_to_return[ 2 ][ 2 ] = Z.z;
+    matrix_to_return[ 3 ][ 2 ] = -glm::dot( Z, position );
+    matrix_to_return[ 0 ][ 3 ] = 0;
+    matrix_to_return[ 1 ][ 3 ] = 0;
+    matrix_to_return[ 2 ][ 3 ] = 0;
+    matrix_to_return[ 3 ][ 3 ] = 1.0f;
 
     return matrix_to_return;
 }
@@ -86,16 +86,16 @@ glm::mat4 Manualtrans::lookAt( glm::vec3 const& position, glm::vec3 const& targe
 // Source : https://github.com/g-truc/glm/blob/0.9.5/glm/gtc/matrix_transform.inl#L208
 glm::mat4 Manualtrans::perspective( float fov, float aspect, float near, float far ) const
 {
-    glm::mat4 matrix_to_return = glm::mat4{0.f};
+    glm::mat4 matrix_to_return = glm::mat4{1.f};
 
-    float rad = fov;
-    float cotanHalfFovy = 1 / std::tan(rad / 2);
+    float rad           = fov;
+    float cotanHalfFovy = 1 / std::tan( rad / 2 );
 
-    matrix_to_return[0][0] = cotanHalfFovy / aspect;
-    matrix_to_return[1][1] = cotanHalfFovy;
-    matrix_to_return[2][2] = (far + near) / (far - near);
-    matrix_to_return[2][3] = -1;
-    matrix_to_return[3][2] = - (2 * far * near) / (far - near);
+    matrix_to_return[ 0 ][ 0 ] = cotanHalfFovy / aspect;
+    matrix_to_return[ 1 ][ 1 ] = cotanHalfFovy;
+    matrix_to_return[ 2 ][ 2 ] = ( far + near ) / ( far - near );
+    matrix_to_return[ 2 ][ 3 ] = -1;
+    matrix_to_return[ 3 ][ 2 ] = -( 2 * far * near ) / ( far - near );
 
     return matrix_to_return;
 }
